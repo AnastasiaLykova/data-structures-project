e6b203c3320041d2fd27e7a39745327a3cc7cfd9
anastasia_lykova <anastasia.7@mail.ru>
2023-06-08 22:32:43 +0500

    testing stack.py 86% coverage

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,18 @@
-"""Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Node, Stack
+
+
+class TestNode(unittest.TestCase):
+
+    def test_init(self):
+        n1 = Node(5, None)
+        self.assertEqual(n1.next_node, None)
+        self.assertEqual(n1.data, 5)
+
+
+class TestStack(unittest.TestCase):
+    def test_push(self):
+        stack = Stack()
+        stack.push('data1')
+        self.assertEqual(stack.top.data, 'data1')
+        self.assertEqual(stack.top.next_node, None)
