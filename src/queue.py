78cc640d0f90e2c22cc6817ced589f4623cc0f4a
anastasia_lykova <anastasia.7@mail.ru>
2023-06-15 01:11:21 +0500

    made __init__ in Node and Queue classes

--- src/queue.py
+++ src/queue.py
@@ -7,7 +7,8 @@ class Node:
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Queue:
@@ -15,7 +16,8 @@ class Queue:
 
     def __init__(self):
         """Конструктор класса Queue"""
-        pass
+        self.head = None
+        self.tail = None
 
     def enqueue(self, data):
         """
